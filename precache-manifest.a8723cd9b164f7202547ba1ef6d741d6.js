7f52af9c93d411c7fcad7a002402443f0e37daf2
Brice  Arnaud Habenicht <arnonono@Brices-iMac.fritz.box>
2020-02-10 13:53:53 +0100

    Updates

---
+++ precache-manifest.a8723cd9b164f7202547ba1ef6d741d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5342cb2753923bcc5e141d4e0796d5d",
+    "url": "/monsters-ag/index.html"
+  },
+  {
+    "revision": "bb1696f74246095c0308",
+    "url": "/monsters-ag/static/css/main.06e418c9.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-ag/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-ag/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb1696f74246095c0308",
+    "url": "/monsters-ag/static/js/main.5fa75a75.chunk.js"
+  },
+  {
+    "revision": "b5f595c9120d6985a463",
+    "url": "/monsters-ag/static/js/runtime-main.649fc334.js"
+  }
+]);
